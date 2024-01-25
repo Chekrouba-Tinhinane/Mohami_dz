851c18e1f9e5be6190657604542ce1f846d2e7b5
ImedBousakhria <i_bousakhria@estin.dz>
2024-01-25 19:24:09 +0100

    feat: profile done

---
+++ .history/client/src/LandingPage_20240125185335.jsx
@@ -0,0 +1,380 @@
+import React, { useEffect, useState } from "react";
+
+import header from "./assets/landing/header.jpg";
+import hero from "./assets/landing/hero.png";
+import section1 from "./assets/landing/section1.jpg";
+import section2 from "./assets/landing/section2.jpg";
+import footer from "./assets/landing/footer.jpg";
+import maria from "./assets/landing/maria.jpg";
+import scene from "./assets/landing/scene.png";
+
+import msg from "./assets/icons/contact/msg.svg";
+
+import twitter from "./assets/icons/footer/twitter.svg";
+import fb from "./assets/icons/footer/fb.svg";
+import linkedin from "./assets/icons/footer/linkedin.svg";
+
+import NavBar from "./components/super/NavBar";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
+
+const LandingPage = () => {
+  const navLinks = [
+    { label: "Accueil", id: "about", offset: -50 },
+    { label: "Trouver un avocat", id: "hero", offset: 0 },
+    { label: "Prendre un rendez-vous", id: "appoint", offset: 50 },
+    { label: "Contactez nous", id: "footer", offset: -100 }
+  ];
+  useEffect(() => {
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="overflow-hidden flex flex-col relative mx-auto items-center bg-white z-10">
+      <header className=" p-3 recursive border-b border-b-primary w-[90%] text-center font-semibold text-xl">
+        {" "}
+        DZ Mouhami{" "}
+      </header>
+      <NavBar links={navLinks} landing={True} />
+      <Header />
+      <About />
+      <Feedback />
+
+      <Achievements />
+      <Hero />
+      <Appoint />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
+
+function Header() {
+  return (
+    <section
+      id="header"
+      className=" relative flex w-screen justify-center my-3"
+    >
+      <img src={header} className="w-full h-[600px] object-cover" alt="" />
+      <h2 className=" recursive absolute bottom-[7%] left-[10%] text-white w-[60%]">
+        "Legal Solutions, Trusted Advocacy: Your Path to Justice Begins with
+        Us."
+      </h2>
+    </section>
+  );
+}
+
+function About() {
+  return (
+    <section
+      id="about"
+      data-aos="fade-right"
+      data-aos-duration="1000"
+      className="mx-3 my-10 flex gap-[6rem] justify-center items-center"
+    >
+      {/* fifth section */}
+      <div className=" flex flex-col w-[40%] ">
+        <h2 className=" recursive w-[40%] place-self-end">
+          A propos de DZ Mouhami
+        </h2>
+        <img src={hero} className="" alt="" />
+      </div>
+
+      <div className=" basis-[40%] mb-16 ">
+        {" "}
+        {/*  */}
+        <h2 className=" recursive tracking-wider w-[50%] my-4">DZ-Mouhami</h2>
+        <p className=" w-[80%] opacity-60 font-light leading-relaxed mb-7">
+          DZMouhami est un annuaire web d'avocats, vous offrant la possibilité
+          de trouver rapidement et facilement un professionnel du droit dans une
+          région spécifique, spécialisé dans un domaine particulier.
+        </p>
+        <a
+          href=""
+          className=" py-2 px-5 text-white bg-primary hover:bg-opacity-65"
+        >
+          Trouver un avocat
+        </a>
+      </div>
+    </section>
+  );
+}
+
+function Feedback() {
+  const userComments = [
+    {
+      username: "User123",
+      comment:
+        "Effortlessly found the right lawyer for my needs – the detailed profiles and client reviews streamlined the decision-making process.",
+    },
+    {
+      username: "LegalEagle",
+      comment:
+        "Incredibly user-friendly directory; comprehensive lawyer profiles and responsive design made my search for legal representation hassle-free.",
+    },
+    {
+      username: "LawSeeker",
+      comment:
+        "Highly impressed with the intuitive interface and wealth of information – this directory simplified the task of finding a skilled attorney.",
+    },
+  ];
+
+  // You can now use the userComments array as needed in your code.
+
+  return (
+    <section
+      id="feedback"
+      className=" relative flex w-screen justify-center my-4"
+    >
+      <img
+        src={section1}
+        className="w-full h-[640px] bg-center object-cover"
+        alt=""
+      />
+      <div className="absolute top-0 text-white">
+        <div className=" flex flex-col items-center">
+          <div className="flex items-center my-5 gap-4">
+            <span className=" font-semibold select-none">____________</span>
+            <h2 className=" recursive  tracking-wider ">
+              Nos Témoignages
+            </h2>{" "}
+            <span className=" font-semibold select-none">____________</span>
+          </div>
+
+          <h2 className=" mt-16 mb-8 recursive w-[85%] text-center">
+            CE QUE NOS CLIENTS PENSENT DE NOUS
+          </h2>
+          <div className=" flex gap-10 w-[75%] ">
+            {userComments.map((el, i) => {
+              return (
+                <div className=" flex flex-col justify-between bg-white p-6 rounded-md text-black text-sm h-56 ">
+                  {el.comment}
+                  <div className=" flex gap-5 items-center">
+                    {" "}
+                    <div class="w-14 h-14 bg-gray-500 rounded-full"></div>
+                    {el.username}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Achievements() {
+  return (
+    <div className=" flex flex-col w-[80%] items-center mx-auto my-10 gap-8 ">
+      <div className="flex items-center my-5 gap-4">
+        <span className=" font-semibold select-none text-primary">
+          ____________
+        </span>
+        <h2 className=" recursive tracking-wider">Nos Réalisations</h2>{" "}
+        <span className=" font-semibold select-none text-primary">
+          ____________
+        </span>
+      </div>
+
+      <div className="flex w-full justify-between  ">
+        <div className="flex flex-col items-center basis-[33%] gap-4">
+          {" "}
+          <h4 className=" recursive text-primary text-3xl font-bold">
+            879+
+          </h4>{" "}
+          <span className=" w-[70%] text-center recursive font-semibold text-xl">
+            Consultations
+          </span>
+        </div>
+        <div className="flex flex-col items-center basis-[33%] gap-4">
+          {" "}
+          <h4 className=" recursive text-primary text-3xl font-bold">
+            12 Mil
+          </h4>{" "}
+          <span className=" w-[70%] text-center recursive font-semibold text-xl">
+            Coûts recouvrés au bénéfice du client
+          </span>
+        </div>
+        <div className="flex flex-col items-center basis-[33%] gap-4">
+          {" "}
+          <h4 className=" recursive text-primary text-3xl font-bold">
+            92%
+          </h4>{" "}
+          <span className=" w-[70%] text-center recursive font-semibold text-xl">
+            Affaires pénales défendues avec succès
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <section id="hero" className=" relative flex w-screen justify-center my-8 ">
+      <img
+        src={section2}
+        className="w-full h-[680px] bg-center object-cover"
+        alt=""
+      />
+      <div className="absolute top-0 text-white">
+        <div className=" flex flex-col items-center gap-[6rem]">
+          <div className="flex items-center my-5 gap-4">
+            <span className=" font-semibold select-none">____________</span>
+            <h2 className=" recursive  tracking-wider ">
+              Parmi nos meilleurs avocats
+            </h2>{" "}
+            <span className=" font-semibold select-none">____________</span>
+          </div>
+
+          <div className=" flex gap-8 w-[80%] bg-white text-black h-[26rem] px-10 my-4 ">
+            <div className="relative basis-[30%]">
+              {" "}
+              <img
+                src={maria}
+                alt=""
+                className=" absolute -top-12  object-contain"
+              />
+            </div>
+            <div className=" flex flex-col basis-[70%] my-4 gap-6  ">
+              {" "}
+              <h2 className=" recursive text-primary">Maria Imene</h2>{" "}
+              <p className=" w-[70%] flex flex-col gap-5 ">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.{" "}
+                </span>
+
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit.
+                </span>
+              </p>
+              <a
+                href=""
+                className="flex w-max recursive text-white bg-primary px-6 py-3 font-semibold hover:bg-opacity-75"
+              >
+                View Profile
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  const [language, setLanguage] = useState("fr");
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+    // You can add logic here to change the language of your website
+    // For example, you might reload the page or fetch translated content from a server
+  };
+  return (
+    <footer
+      id="footer"
+      className="h-[320px] relative flex w-full justify-center mt-[10rem] text-white"
+    >
+      <img src={footer} alt="" className="w-full object-cover" />
+      {/* sixth section */}
+      <div className="absolute w-full flex justify-center py-[4rem]">
+        <div className="flex items-center justify-between px-16 max-w-[86rem] w-full">
+          <div className="flex flex-col gap-6">
+            <h3 className=" text-2xl">AIDE?</h3>
+            <ul className="flex flex-col font-light gap-2">
+              <li>FAQ</li>
+              <li>PRIVACY POLICY</li>
+              <li>Termes et Conditions</li>
+              <li>Contactez nous</li>
+            </ul>
+
+            <div className="language-selector font-light">
+              <label htmlFor="language-select">Language:</label>
+              <select
+                id="language-select"
+                value={language}
+                onChange={handleLanguageChange}
+              >
+                <option value="fr">Français</option>
+                <option value="en">English</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="flex place-self-start gap-[3rem]">
+            <img src={twitter} alt="" />
+            <img src={fb} alt="" />
+            <img src={linkedin} alt="" />
+          </div>
+
+          <div className="border-l border-l-white pl-10 flex flex-col gap-5 h-[85%] place-self-start">
+            <p className="flex gap-2">
+              CONTACTEZ NOUS
+              <span>
+                <img src={msg} alt="" />
+              </span>
+            </p>
+            <div className="flex flex-col gap-2 font-light w-[80%]">
+              <p>Rue Cheikh Ammar, Béjaia, Algeria</p>
+              <p>Tél 1-888-888-888</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+function Appoint() {
+  return (
+    <section
+      id="appoint"
+      className=" flex flex-col w-full justify-center items-center gap-[3rem] my-10"
+    >
+      <div className=" flex flex-col items-center">
+        <div className="flex items-center my-5 gap-4">
+          <span className=" font-semibold select-none text-primary">
+            ____________
+          </span>
+          <h2 className=" recursive  tracking-wider ">Prise de Rendez-Vous</h2>{" "}
+          <span className=" font-semibold select-none text-primary">
+            ____________
+          </span>
+        </div>
+      </div>
+
+      <div className=" flex w-[80%] items-center justify-between ">
+        <div className=" basis-[40%] mb-16 ">
+          {" "}
+          {/*  */}
+          <h2 className=" recursive tracking-wider w-[50%] my-4">DZ-Mouhami</h2>
+          <p className=" w-[80%] opacity-60 font-light leading-relaxed mb-7">
+            DZMouhami est un annuaire web d'avocats, vous offrant la possibilité
+            de trouver rapidement et facilement un professionnel du droit dans
+            une région spécifique, spécialisé dans un domaine particulier.
+          </p>
+          <a
+            href=""
+            className=" py-3 px-5 text-white bg-primary hover:bg-opacity-65"
+          >
+            Prendre un rendez-vous{" "}
+          </a>
+        </div>
+
+        <img src={scene} className="" alt="" />
+      </div>
+    </section>
+  );
+}
---
+++ .history/client/src/components/Profile_20240125183332.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import msg from "../assets/icons/contact/msg.svg";
+import phone from "../assets/icons/contact/phone.svg";
+
+const Profile = () => {
+  return (
+    <div className=" mx-4 bg-lightBrown min-h-max">
+      <Coords />{" "}
+    </div>
+  );
+};
+
+export default Profile;
+
+function Coords() {
+  return (
+    <div className="flex py-3 px-8">
+      <div className=" basis-[70%] flex flex-col gap-4 ">
+        <div className="flex flex-col gap-2">
+          {" "}
+          <h2>William King</h2>
+          <h4>Region, Ville, 11000</h4>
+        </div>
+        <div className="flex ">
+          <div className=" bg-gray-300 h-56 w-56"></div>
+          <div className="flex flex-col">
+            {" "}
+            Rating here
+            <p>
+              Spécialité: Droits administratifs{" "}
+              <a href="" className=" text-primary underline">
+                Voir plus
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className=" flex-col basis-[30%] border-l border-l-primary">
+        <div className=" flex items-center ">
+          {" "}
+          <img src={msg} alt="" />
+          Email :email_81194@gmail.com{" "}
+        </div>
+        <div className=" flex items-center ">
+          {" "}
+          <img src={phone} alt="" />
+          Numéro de téléphone : 000-000-000{" "}
+        </div>
+        <p className="text-primary underline">HP Law, PLLC</p>
+        <div className="flex flex-col h-full">
+          {" "}
+          <button className="bg-primary p-3 place-self-end">
+            Prendre Rendez-Vous
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
