65d626fdb5f38176de1cc03198f1a2a7b73e6cd8
yaCHBN2004 <yacinecheurfi00@gmail.com>
2024-02-01 02:48:40 +0100

    feat: done translating signup

--- client/src/components/super/NavBar.jsx
+++ client/src/components/super/NavBar.jsx
@@ -2,8 +2,11 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { useUserData } from "../../App";
+import { useTranslation } from "react-i18next";
 
-const NavBar = ({ links, landing }) => {
+const NavBar = ({ links, landing, l }) => {
+  const { t } = useTranslation(); // Use useTranslation hook to access translation function
+
   const { userData } = useUserData();
   const logged = userData == undefined;
   return (
@@ -22,52 +25,56 @@ const NavBar = ({ links, landing }) => {
             transition={{ type: "tween", duration: 0.2 }}
             className="font-semibold opacity-80 tracking-wider cursor-pointer"
           >
-            <ScrollLink
-              to={link.id}
-              spy={true}
-              smooth={true}
-              offset={link.offset}
-              duration={500}
-              className="nav-item"
-            >
-              {link.label}
-            </ScrollLink>
+            {l ? (
+              <ScrollLink
+                to={link.id}
+                spy={true}
+                smooth={true}
+                offset={link.offset}
+                duration={500}
+                className="nav-item"
+              >
+                {link.label}
+              </ScrollLink>
+            ) : (
+              <Link to={link.to}>{link.label}</Link>
+            )}
           </motion.li>
         ))}
       </motion.ul>
-      { landing ? (
-          logged ? (
-            <div className=" space-x-6">
-          <div className="flex gap-[1rem]">
-            <Link
-              to={"/SignIn"}
-              href=""
-              className="recursive flex items-center font-semibold text-primary border border-primary bg-white text-center text-lg py-1.5 px-3.5 hover:bg-opacity-70"
-            >
-              Se connecter
-            </Link>
+      {landing ? (
+        logged ? (
+          <div className=" space-x-6">
             <div className="flex gap-[1rem]">
-            <Link
-              to={"/SignUp"}
-              href=""
-              className="recursive flex items-center font-semibold text-primary border border-primary bg-white text-center text-lg py-1.5 px-3.5 hover:bg-opacity-70"
-            >
-              S'inscrire
-            </Link>
-          </div>
-          </div>
+              <Link
+                to={"/SignIn"}
+                href=""
+                className="recursive flex items-center font-semibold text-primary border border-primary bg-white text-center text-lg py-1.5 px-3.5 hover:bg-opacity-70"
+              >
+                {t("connexion")}
+              </Link>
+              <div className="flex gap-[1rem]">
+                <Link
+                  to={"/SignUp"}
+                  href=""
+                  className="recursive flex items-center font-semibold text-primary border border-primary bg-white text-center text-lg py-1.5 px-3.5 hover:bg-opacity-70"
+                >
+                  {t("inscription")}
+                </Link>
+              </div>
+            </div>
           </div>
         ) : (
-        <Link
-          to={"/SignIn"}
-          onClick={() => console.log("removeToken")}
-          href=""
-          className="recursive flex items-center font-semibold text-primary border border-primary bg-white text-center text-lg py-1.5 px-3.5 hover:bg-opacity-70"
-        >
-          Se déconnecter
-        </Link>
-      )
-      ) : null }
+          <Link
+            to={"/SignIn"}
+            onClick={() => console.log("removeToken")}
+            href=""
+            className="recursive flex items-center font-semibold text-primary border border-primary bg-white text-center text-lg py-1.5 px-3.5 hover:bg-opacity-70"
+          >
+            {t("déconnexion")}
+          </Link>
+        )
+      ) : null}
     </div>
   );
 };
