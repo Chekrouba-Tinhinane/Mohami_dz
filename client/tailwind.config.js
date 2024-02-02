87278f6983d793839a1aacc348da5414ce787e86
Imed Bousakhria <i_bousakhria@estin.dz>
2024-02-02 21:26:56 +0100

    refractor: phone screen setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,12 +6,14 @@ module.exports = {
       colors: {
         lightBrown: "#FAF5EE",
         primary: "#D4AD6B",
-        lightTypo: "#563D2B"
+        lightTypo: "#563D2B",
       },
-    },
-    screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 690px) { ... }
+        phone: "320px",
+        // => @media (min-width: 690px) { ... }
+      },
     },
   },
   plugins: [],
